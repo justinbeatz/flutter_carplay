c64219cc8a579d1b0dcdf3f389a820db0190874b
Michał Wyczarski <mwyczarski@mobitouch.net>
2023-06-13 10:41:24 +0200

    add a way to update now playing buttons

--- lib/models/button/now_playing_button.dart
+++ lib/models/button/now_playing_button.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_carplay/carplay_worker.dart';
+import 'package:flutter_carplay/controllers/carplay_controller.dart';
 import 'package:uuid/uuid.dart';
 
 class CPNowPlayingButton {
@@ -8,7 +10,7 @@ class CPNowPlayingButton {
 
   /// Displayed inside the button with a big padding
   /// I recommend using square png icons
-  final String? image;
+  String? image;
 
   /// Fired when the user taps the button.
   final Function(CPNowPlayingButton self)? onPress;
@@ -29,6 +31,11 @@ class CPNowPlayingButton {
 
   String get uniqueId {
     return _elementId;
+  }
+
+  void updateImage(String image) {
+    this.image = image;
+    FlutterCarPlayController.updateCPNowPlayingButton(this);
   }
 }
 
